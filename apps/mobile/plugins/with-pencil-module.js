95756abda18dd5bf100614174f8506da1069eec9
Toprak Demirel <4toprak25@gmail.com>
2026-04-12 22:53:23 +0200

    fix(mobile): resolve expo config plugin import for iOS build

--- apps/mobile/plugins/with-pencil-module.js
+++ apps/mobile/plugins/with-pencil-module.js
@@ -23,7 +23,7 @@
 
 const fs = require('fs');
 const path = require('path');
-const { withXcodeProject, withDangerousMod } = require('@expo/config-plugins');
+const { withXcodeProject, withDangerousMod } = require('expo/config-plugins');
 
 const SOURCES = [
   'PencilInteractionModule.swift',
